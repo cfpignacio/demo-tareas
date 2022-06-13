1dfcf5c669f4d539ed349345899339d79f005fea
Ignaciom <45469115+igncam@users.noreply.github.com>
2022-06-13 20:43:59 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function guardarTarea(){
+
+    var tareaInput = document.getElementById("input-tarea")
+
+    var listado = document.getElementById("listado-tareas")
+
+    listado.innerHTML += `<li>${tareaInput.value}</li>`
+
+    tareaInput.value = ""
+
+}
