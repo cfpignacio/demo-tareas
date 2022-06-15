800d0d42ec8a9ab31eab9bb37e18a875747301a8
Ignaciom <45469115+igncam@users.noreply.github.com>
2022-06-15 20:39:52 -0300

    add bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,48 @@
-function guardarTarea(){
+// function guardarTarea(){
 
-    var tareaInput = document.getElementById("input-tarea")
+//     var tareaInput = document.getElementById("input-tarea")
 
-    var listado = document.getElementById("listado-tareas")
+//     var listado = document.getElementById("listado-tareas")
 
-    listado.innerHTML += `<li>${tareaInput.value}</li>`
+//     listado.innerHTML += `<li>${tareaInput.value}</li>`
 
-    tareaInput.value = ""
+//     tareaInput.value = ""
+
+// }
+
+var listadoTareas = [];
+
+document.addEventListener("submit", function(event){
+
+    event.preventDefault();
+    var tarea = event.target[0].value
+
+    if(tarea == ""){
+    return alert("Por favor completar el campo tarea")
+    }
+
+    listadoTareas.push(tarea)
+
+   
+
+    event.target[0].value = ""
+    addTareaHtml()
+})
+
+
+function addTareaHtml(){
+    // for (let index = 0; index < listadoTareas.length; index++) {
+    //    console.log(listadoTareas[index])
+    // }
+    // listadoTareas.forEach(function(e){
+    //     console.log(e)
+    // })
+    var listadoHTML = document.getElementById("listado-tareas")
+    // listadoTareas.map(function(t){
+    //     listadoHTML.innerHTML += `<li>${t}</li>`
+    // })
+    const ultimaTarea = listadoTareas[listadoTareas.length - 1]
+    listadoHTML.innerHTML += `<li>${ultimaTarea}</li>`
+
 
 }
